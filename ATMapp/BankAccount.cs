5fca58b541d4478d9e389b515b937d459b10d616
Petr-Senfeld <59889161+Petr-Senfeld@users.noreply.github.com>
2020-01-20 15:39:29 +0100

    Add files via upload

---
+++ ATMapp/BankAccount.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        public string Number { get;  }
+        public string Owner { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+
+        }
+
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            else
+            {
+                var deposit = new Transaction(amount, date, note);
+                allTransactions.Add(deposit);
+            }
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+                if (amount <= 0)
+                {
+                    Console.WriteLine("Amount of withdrawal must be positive");
+                    Console.WriteLine("Please try again..");
+                    //throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+                }
+                else if (Balance - amount < 0)
+                {
+                    Console.WriteLine("Not sufficient funds for this withdrawal");
+                    Console.WriteLine("Please try again..");
+                    //throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+                }
+                else
+                {
+                    var withdrawal = new Transaction(-amount, date, note);
+                    allTransactions.Add(withdrawal);
+                }
+        }
+
+        public void Transfer(decimal amount, BankAccount prijemce ,DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of tranfer must be positive");
+            }
+            else if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this tranfer");
+            }
+            else
+            {
+                var transfer = new Transaction(-amount, date, note);
+                prijemce.MakeDeposit(amount, DateTime.Now, note);
+                //MakeWithdrawal(amount, DateTime.Now, note);
+                allTransactions.Add(transfer);
+            }
+
+        }
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            Console.WriteLine($"Balance history of {Owner}");
+            report.AppendLine("Date     \tAmount \tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+    }
+}
---
+++ ATMapp/transaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    public class Transaction
+    {
+        public string User { get; }
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal Amount, DateTime date, string note)
+        {
+            this.Amount = Amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+
+    }
+    public class user_Transaction
+    {
+        public string User { get; }
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public user_Transaction(decimal Amount, DateTime date, string note)
+
+        {
+            this.Amount = Amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
